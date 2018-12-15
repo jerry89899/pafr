ca04ee4764f108e8fd1ffaed01225ffde2f18872
jerry89899 <jerrylooman@hotmail.com>
2018-12-15 16:23:42 +0100

    gui added and trains

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("../presentation/main.fxml"));
+        primaryStage.setTitle("Rich rail");
+        primaryStage.setScene(new Scene(root, 640, 600));
+        primaryStage.getScene().getStylesheets().add("/presentation/bootstrap.css");
+        primaryStage.show();
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
