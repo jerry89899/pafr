bebc6a31ff18ca93aaa4e919f2561994eed05e23
allxclusive <39024482+allxclusive@users.noreply.github.com>
2018-12-15 16:04:02 +0100

    fix in demo

--- src/Application/TrainRepository.java
+++ src/Application/TrainRepository.java
@@ -3,4 +3,7 @@ package Application;
 public class TrainRepository implements TrainRepositoryInterface {
 
     // hier een array met alle treinen waar je de treinen in modified ?
+    //hier geef je aan welke databaseconnection je wilt gebruiken zodat dat niet in de service hoeft aangegeven te worden
+
+
 }
--- src/Application/start.java
+++ src/Application/start.java
@@ -13,7 +13,7 @@ public class start {
         ts.getTrains().get(0).addWagon(ts.getRollingComponents().get(2));
         System.out.println(ts.getTrains());
         System.out.println(ts.getTrains().get(0).getRollingComponents());
-
+        ts.NewTrain(ts.getRollingComponents().get(0), "test2");
         //        ts.NewTrain();
     }
 }
